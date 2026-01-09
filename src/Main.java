45d23d410a8bddd3ef86e3630a2c50f8ec618b19
Bảo Dư <duybao1403@gmail.com>
2026-01-10 00:03:51 +0700

    Adding two pointer approach with O(n) time and O(1) space - isValidSubsequence2()

--- src/Main.java
+++ src/Main.java
@@ -57,4 +57,17 @@ public class Main {
         }
         return true;
     }
+
+    //remember: order matter
+    public static boolean isValidSubsequence2(List<Integer> array, List<Integer> sequence){
+        int arrIndex = 0;
+        int seqIndex = 0;
+        while (arrIndex < array.size() && seqIndex < sequence.size()){
+            if(array.get(arrIndex).equals(sequence.get(seqIndex))){
+                seqIndex++;
+            }
+            arrIndex++;
+        }
+        return seqIndex == sequence.size();
+    }
 }
